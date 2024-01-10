1886f5fae5923e567bd4f6b5f7bbe81a2b08e13b
Steven Chang <stevenchang5000@protonmail.com>
2024-01-11 09:57:09 +1000

    Cheques#index: init

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,28 @@ import "./src/jquery";
 import "../../lib/assets/js/layout";
 import "../../lib/assets/js/app";
 
+// === CUSTOM ===
+import { CHEQUES_INDEX } from "./cheques/index";
+
 // === GLOBAL LISTENERS ===
 $(document).on("turbo:load", function () {
   $("html").attr("data-preloader", "enable");
-  // Set this up in separate pages where required
-	$("html").attr("data-preloader", "disable");
+  if ($("#cheques-index").length) {
+    CHEQUES_INDEX.init();
+  }
 });
+
+// === HELPERS ===
+export const HELPERS = {
+  cryptocurrencies: undefined,
+  getCryptocurrencies: async() => {
+    if (!HELPERS.cryptocurrencies) {
+      HELPERS.cryptocurrencies = await $.ajax({
+        url: `https://btn.group/cryptocurrencies?blockchain_id=185&official=true&include_attachments=true`,
+        type: "get",
+        dataType: "json",
+      });
+    }
+    return HELPERS.cryptocurrencies;
+  }
+};
