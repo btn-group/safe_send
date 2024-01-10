1b3c0207e5454ef8d9082b9ad925b394417f4319
Steven Chang <stevenchang5000@protonmail.com>
2024-01-11 08:46:16 +1000

    Add Velzon theme; Init home;

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,13 @@
-// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+import "./src/jquery";
+
+// === VELZON ===
+import "../../lib/assets/js/layout";
+import "../../lib/assets/js/app";
+
+// === GLOBAL LISTENERS ===
+$(document).on("turbo:load", function () {
+  $("html").attr("data-preloader", "enable");
+  // Set this up in separate pages where required
+	$("html").attr("data-preloader", "disable");
+});
