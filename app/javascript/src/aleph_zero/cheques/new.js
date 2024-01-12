01b946a24ca4d82594384d9ee760ccf06023f3e1
Lucy Berwick <91242746+lucy-berwick@users.noreply.github.com>
2024-01-12 13:25:34 +1000

    Cheques#new: setFee (#12)

--- app/javascript/src/aleph_zero/cheques/new.js
+++ app/javascript/src/aleph_zero/cheques/new.js
@@ -6,8 +6,33 @@ export const CHEQUES_NEW = {
   init: async () => {
     let cryptocurrencies = await HELPERS.getCryptocurrencies();
     console.log(cryptocurrencies);
+    CHEQUES_NEW.setFee();
     $("html").attr("data-preloader", "disable");
     POLKADOTJS.listenForConnectButtonClick(ALEPH_ZERO);
     await ALEPH_ZERO.activatePolkadotJsExtension();
   },
+  setFee: async () => {
+      try {
+        const contract = await ALEPH_ZERO.contracts[
+          "safeSend"
+        ].getContract();
+        let api = await ALEPH_ZERO.api();
+        let response = await POLKADOTJS.contractQuery(
+          api,
+          ALEPH_ZERO.b3,
+          contract,
+          "config",
+          undefined,
+          []
+        );
+        let fee = parseFloat(response.output.asOk.toHuman().fee.replace(/,/g, '')) / 1_000_000_000_000;
+        if (HELPERS.environment == 'production') {
+          $("#fee-amount").text(`${fee} AZERO`)
+        } else {
+          $("#fee-amount").text(`${fee} TZERO`)
+        }
+      } catch (err) {
+        document.showAlertDanger(err);
+      }
+  }
 };
