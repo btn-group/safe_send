322e7491cf6c5444dd2616399ffc722e39e7d87b
Steven Chang <stevenchang5000@protonmail.com>
2024-01-22 14:11:33 +1000

    subsquid: return by createdAT_DESC

--- app/javascript/src/aleph_zero/helpers.js
+++ app/javascript/src/aleph_zero/helpers.js
@@ -215,7 +215,7 @@ export const ALEPH_ZERO = {
     },
     queryData: () => {
       let query = `query MyQuery {
-        cheques(orderBy: id_DESC, where: {from_eq: "${ALEPH_ZERO.account.address}", OR: {to_eq: "${ALEPH_ZERO.account.address}"}}) {
+        cheques(orderBy: createdAt_DESC, where: {from_eq: "${ALEPH_ZERO.account.address}", OR: {to_eq: "${ALEPH_ZERO.account.address}"}}) {
           id
           from
           to
